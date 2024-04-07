a9eeb5a027d0ebfd9fdbe98cf1e5a1d6d6a5db52
leidZhang <haoz28889@gmail.com>
2024-04-07 01:21:34 -0400

    utils

--- examples/spawn_demo.py
+++ examples/spawn_demo.py
@@ -2,7 +2,6 @@
 import numpy as np
 # quanser imports
 from qvl.qlabs import QuanserInteractiveLabs
-# from qvl.basic_shape import QLabsBasicShape
 from hal.utilities.path_planning import RoadMapNode
 # custom imports
 from qenv.qlab.env import ACCRoadMap
@@ -19,10 +18,3 @@ def spawn_demo(id=24) -> None:
     # qlabs.open("localhost")
     # node_sequence: list = [10, 17, 10]
     # waypoint_sequence: np.ndarray = roadmap.generate_path(node_sequence)
-
-    # rotation:list = [0, 0, 0]
-    # scale:list = [0.01, 0.01, 0.01]
-    # basic_shape: QLabsBasicShape = QLabsBasicShape(qlabs=qlabs, verbose=False)
-    # for i in range(len(waypoint_sequence[0])):
-    #     location = [waypoint_sequence[0][i], waypoint_sequence[1][i], 1]
-    #     basic_shape.spawn_id(i, location=location, rotation=rotation, scale=scale, configuration=1, waitForConfirmation=False)
--- qenv/utils/qlab_utlis.py
+++ qenv/utils/qlab_utlis.py
@@ -1,5 +1,18 @@
+# 3rd party imports
+import numpy as np
+# quanser imports
 from qvl.qlabs import QuanserInteractiveLabs
+from qvl.basic_shape import QLabsBasicShape
 
-def connect_to_qlab() -> None:
+def connect_to_qlab() -> QuanserInteractiveLabs:
     qlabs = QuanserInteractiveLabs()
     qlabs.open("localhost")
+    return qlabs
+
+def generate_waypoints(qlabs: QuanserInteractiveLabs, sequence: np.ndarray) -> None:
+    rotation:list = [0, 0, 0]
+    scale:list = [0.01, 0.01, 0.01]
+    basic_shape: QLabsBasicShape = QLabsBasicShape(qlabs=qlabs, verbose=False)
+    for i in range(len(sequence[0])):
+        location = [sequence[0][i], sequence[1][i], 1]
+        basic_shape.spawn_id(i, location=location, rotation=rotation, scale=scale, configuration=1, waitForConfirmation=False)
